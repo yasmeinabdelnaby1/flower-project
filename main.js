646f0e9de414ca2e5dc853d519b65d9bc45304b0
yasmeinabdelnaby1 <yasmeinabdalnaby@gmail.com>
2025-07-05 22:49:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const navLinks = document.querySelector(".nav-links");
+
+function toggleMenuFunc() {
+  navLinks.classList.toggle("show");
+}
+
+toggleMenu.addEventListener("click", toggleMenuFunc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
